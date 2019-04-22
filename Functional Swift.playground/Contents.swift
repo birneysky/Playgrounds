d2ec5088c7da69b060b48f746dc9159677be68c2
yingwei0831 <birneysky@163.com>
2019-04-23 00:16:00 +0800

    m

--- Functional Swift.playground/Contents.swift
+++ Functional Swift.playground/Contents.swift
@@ -7,8 +7,8 @@ var str = "Hello, playground"
 let filterNames = CIFilter.filterNames(inCategory: kCICategoryBuiltIn)
 let lvc = FilterListViewController(style: .plain)
 let rootNav = UINavigationController(rootViewController: lvc)
+rootNav.view.frame = CGRect(origin: CGPoint.zero, size: CGSize(width: 320, height: 640))
 PlaygroundPage.current.liveView = rootNav.view
-
 
 //let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 400, height: 600))
 //
