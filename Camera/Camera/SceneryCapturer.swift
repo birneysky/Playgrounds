8bdc7b1763898609bb03c0b81be7bdb7f0a95d56
🐯🍎💦🌟🍀 <birneysky@163.com>
2019-05-08 23:27:12 +0800

    add project camera

---
+++ Camera/Camera/SceneryCapturer.swift
@@ -0,0 +1,25 @@
+//
+//  SceneryCapturer.swift
+//  Camera
+//
+//  Created by birneysky on 2019/5/8.
+//  Copyright © 2019 Grocery. All rights reserved.
+//
+
+import UIKit
+class SceneryCapturer {
+    
+    func startRunning() {
+        
+    }
+    
+    func stopRunning() {
+        
+    }
+    
+    func focusAtPosition(_ point:CGPoint) {
+        
+    }
+    
+    
+}
