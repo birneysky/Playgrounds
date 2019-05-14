3239ed575114b92d5a29438d6a9998c50f0a7e92
zhaobingdong🌲🌱🌿💥 <zhaobingdong@rongcloud.cn>
2019-05-14 18:39:41 +0800

    modify SceneryCapturer

--- Camera/Camera/SceneryCapturer.swift
+++ Camera/Camera/SceneryCapturer.swift
@@ -6,20 +6,95 @@
 //  Copyright © 2019 Grocery. All rights reserved.
 //
 
-import UIKit
-class SceneryCapturer {
+import AVFoundation
+
+final class SceneryCapturer : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
     
-    func startRunning() {
+    public override init() {
+        super.init()
+        setupCaptureSession()
+    }
+    
+    public func startRunning() {
+        if !self.captureSession.isRunning {
+            weak var weakSelf = self;
+            self.sessionQueue.async {
+                weakSelf?.captureSession.startRunning()
+            }
+        }
+    }
+    
+    public func stopRunning() {
+        if self.captureSession.isRunning {
+            weak var weakSelf = self;
+            self.sessionQueue.async {
+                weakSelf?.captureSession.stopRunning()
+            }
+        }
+    }
+    
+    public func focusAtPosition(_ point:CGPoint) {
         
     }
     
-    func stopRunning() {
+    // MARK: - Helper
+    fileprivate func setupCaptureSession() {
+        NotificationCenter.default.addObserver(self, selector: #selector(sessionWasInterrupted(_:)), name: .AVCaptureSessionWasInterrupted, object: nil)
+        do {
+            let videoDeviceInput = try AVCaptureDeviceInput(device: self.videoDevice)
+            if self.captureSession.canAddInput(videoDeviceInput) {
+                self.captureSession.addInput(videoDeviceInput)
+            }
+        } catch let error as NSError {
+            print(error)
+        }
+        
+        let videoDataOutput = AVCaptureVideoDataOutput()
+        videoDataOutput.setSampleBufferDelegate(self, queue: self.sessionQueue)
+        videoDataOutput.alwaysDiscardsLateVideoFrames = false
+        
+        if self.captureSession.canAddOutput(videoDataOutput) {
+            self.captureSession.addOutput(videoDataOutput)
+        }
+        
+        self.captureSession.sessionPreset = .high
+        
+        
+        do {
+            let duration = CMTimeMake(value: 1, timescale: 20)
+            try self.videoDevice.lockForConfiguration()
+            self.videoDevice.activeVideoMaxFrameDuration = duration
+            self.videoDevice.activeVideoMinFrameDuration = duration
+            self.videoDevice.unlockForConfiguration()
+        } catch let error as NSError {
+            print(error)
+        }
         
     }
-    
-    func focusAtPosition(_ point:CGPoint) {
+    // MARK: - AVCaptureVideoDataOutputSampleBufferDelegate
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         
     }
+    // MARK: - Notification selector
+    @objc func sessionWasInterrupted(_ notification: Notification) {
+        print("Capture session was interrupted with reason: \(notification)")
+    }
     
+    // MARK: - Properties
+    fileprivate lazy var videoDevice: AVCaptureDevice = {
+        guard let device = AVCaptureDevice.default(for: .video) else {
+            fatalError("video device is nil")
+        }
+        return device
+    }()
     
+    fileprivate lazy var sessionQueue: DispatchQueue = {
+       let queue = DispatchQueue(label: "com.camera.SceneryCapturer.session")
+        return queue
+    }()
+    
+    fileprivate lazy var captureSession: AVCaptureSession = {
+       let session = AVCaptureSession()
+        return session
+    }()
 }
