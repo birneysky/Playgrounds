c2844a0780b399c6f73ee4a995682f24eb64d876
zhaobingdong🌲🌱🌿💥 <zhaobingdong@rongcloud.cn>
2019-05-22 17:04:35 +0800

    take a small step forward

--- Camera/Camera/SceneryCapturer.swift
+++ Camera/Camera/SceneryCapturer.swift
@@ -14,12 +14,33 @@ protocol SceneryCapturerOutputDelegate: class {
 
 final class SceneryCapturer : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
     
+    
+    // MARK: - Properties
+    public weak var delegate: SceneryCapturerOutputDelegate?
+    fileprivate var videoConnection: AVCaptureConnection!
+    fileprivate lazy var videoDevice: AVCaptureDevice = {
+        guard let device = AVCaptureDevice.default(for: .video) else {
+            fatalError("video device is nil")
+        }
+        return device
+    }()
+    
+    fileprivate lazy var sessionQueue: DispatchQueue = {
+        let queue = DispatchQueue(label: "com.camera.SceneryCapturer.session")
+        return queue
+    }()
+    
+    fileprivate lazy var captureSession: AVCaptureSession = {
+        let session = AVCaptureSession()
+        return session
+    }()
+    
+    
+    // MARK: - Init
     public override init() {
         super.init()
         setupCaptureSession()
     }
-    
-    weak var delegate: SceneryCapturerOutputDelegate?
     
     public func startRunning() {
         if !self.captureSession.isRunning {
@@ -62,6 +83,7 @@ final class SceneryCapturer : NSObject, AVCaptureVideoDataOutputSampleBufferDele
         if self.captureSession.canAddOutput(videoDataOutput) {
             self.captureSession.addOutput(videoDataOutput)
         }
+        self.videoConnection = videoDataOutput.connection(with: .video)
         
         self.captureSession.sessionPreset = .high
         
@@ -76,6 +98,7 @@ final class SceneryCapturer : NSObject, AVCaptureVideoDataOutputSampleBufferDele
             print(error)
         }
         
+        self.videoConnection?.videoOrientation = .portrait
     }
     // MARK: - AVCaptureVideoDataOutputSampleBufferDelegate
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
@@ -85,22 +108,4 @@ final class SceneryCapturer : NSObject, AVCaptureVideoDataOutputSampleBufferDele
     @objc func sessionWasInterrupted(_ notification: Notification) {
         print("Capture session was interrupted with reason: \(notification)")
     }
-    
-    // MARK: - Properties
-    fileprivate lazy var videoDevice: AVCaptureDevice = {
-        guard let device = AVCaptureDevice.default(for: .video) else {
-            fatalError("video device is nil")
-        }
-        return device
-    }()
-    
-    fileprivate lazy var sessionQueue: DispatchQueue = {
-       let queue = DispatchQueue(label: "com.camera.SceneryCapturer.session")
-        return queue
-    }()
-    
-    fileprivate lazy var captureSession: AVCaptureSession = {
-       let session = AVCaptureSession()
-        return session
-    }()
 }
--- Camera/Camera/ViewController.swift
+++ Camera/Camera/ViewController.swift
@@ -14,33 +14,85 @@ import CoreImage
 class ViewController: UIViewController,SceneryCapturerOutputDelegate {
 
     @IBOutlet weak var previewView: SceneryPreviewView!
-    weak var displayLayer: AVSampleBufferDisplayLayer? {
-        return self.previewView.displayLayer
-    }
+    weak var displayLayer: AVSampleBufferDisplayLayer!
+    fileprivate var pixelBufferPool: CVPixelBufferPool!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.displayLayer = self.previewView.displayLayer
         print(CIFilter.filterNames(inCategory: kCICategoryVideo))
         // Do any additional setup after loading the view.
-        
         self.capturer.startRunning()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
 
 
     func didOutputSampleBuffer(_ buffer: CMSampleBuffer) {
-//        guard let imageBuffer = CMSampleBufferGetImageBuffer(buffer) else {
-//            return;
-//        }
-//        let img = CIImage(cvImageBuffer: imageBuffer)
-//        guard let filer = CIFilter.init(name: "CIFaceBalance") else {
-//            return;
-//        }
-//        filer.setValue(img, forKey: "inputImage")
-//        guard let outputImg = filer.outputImage else {
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(buffer) else {
+            return;
+        }
+        let img = CIImage(cvImageBuffer: imageBuffer)
+        guard let filer = CIFilter.init(name: "CIFaceBalance") else {
+            print("filter  is nil")
+            return;
+        }
+        filer.setValue(img, forKey: "inputImage")
+        guard let outputImg = filer.outputImage else {
+            print("filter outputimage is nil")
+            return
+        }
+        
+        let aDuration = CMSampleBufferGetDuration(buffer)
+        let aPts = CMSampleBufferGetPresentationTimeStamp(buffer)
+        let aDts = CMSampleBufferGetDecodeTimeStamp(buffer)
+        var timingInfo = CMSampleTimingInfo(duration: aDuration, presentationTimeStamp: aPts, decodeTimeStamp: aDts)
+        var videoFormat: CMVideoFormatDescription?
+        CMVideoFormatDescriptionCreateForImageBuffer(allocator: kCFAllocatorDefault, imageBuffer: imageBuffer, formatDescriptionOut: &videoFormat)
+        guard let videoFormatDescription = videoFormat else {
+            return
+        }
+        
+        if self.pixelBufferPool == nil {
+            let poolAttributes: [CFString:Any]  = [kCVPixelBufferPoolMinimumBufferCountKey:2]
+            let inputMediaSubType = CMFormatDescriptionGetMediaSubType(videoFormatDescription)
+            
+            let inputDimensions = CMVideoFormatDescriptionGetDimensions(videoFormatDescription)
+            
+            let pixelBufferAttributes: [CFString: Any]  = [
+                kCVPixelBufferPixelFormatTypeKey : inputMediaSubType,
+                kCVPixelBufferWidthKey : inputDimensions.width,
+                kCVPixelBufferHeightKey : inputDimensions.height
+            ]
+            
+            CVPixelBufferPoolCreate(kCFAllocatorDefault, poolAttributes as CFDictionary, pixelBufferAttributes as CFDictionary, &self.pixelBufferPool)
+        }
+        
+        var pbuf: CVPixelBuffer?
+        CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, self.pixelBufferPool, &pbuf)
+        guard let outputPixelBuffer = pbuf else {
+            print("Allocation failure")
+            return
+        }
+        
+
+        self.context.render(outputImg, to: outputPixelBuffer ,bounds: outputImg.extent, colorSpace: CGColorSpaceCreateDeviceRGB())
+//        var outputVideoFormat: CMVideoFormatDescription?
+//        CMVideoFormatDescriptionCreateForImageBuffer(allocator: kCFAllocatorDefault, imageBuffer: outputPixelBuffer, formatDescriptionOut: &outputVideoFormat)
+//        guard let ovfd = videoFormat else {
 //            return
 //        }
+        var finalSampleBuffer: CMSampleBuffer?
+        CMSampleBufferCreateForImageBuffer(allocator: kCFAllocatorDefault, imageBuffer: outputPixelBuffer, dataReady: true, makeDataReadyCallback: nil, refcon: nil, formatDescription: videoFormatDescription, sampleTiming: &timingInfo, sampleBufferOut: &finalSampleBuffer)
         //print(img.autoAdjustmentFilters())
-        self.displayLayer?.enqueue(buffer)
+        //DispatchQueue.main.async {
+        guard let fsb = finalSampleBuffer else {
+            return
+        }
+        self.displayLayer.enqueue(fsb)
+        //}
     }
     
     fileprivate lazy var capturer:  SceneryCapturer = {
@@ -48,5 +100,13 @@ class ViewController: UIViewController,SceneryCapturerOutputDelegate {
         capturer.delegate = self
         return capturer;
     }()
+    
+    fileprivate lazy var context: CIContext = {
+        let eaglContext = EAGLContext(api: EAGLRenderingAPI.openGLES2)
+        let context = CIContext(eaglContext: eaglContext!)
+        return context
+    }()
+    
+    
 }
 
