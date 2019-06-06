aad2621e25b81289958232bf825f3f750e8802d2
🐯🍎💦🌟🍀 <birneysky@163.com>
2019-06-07 00:17:17 +0800

    make a little for server.js

--- node/webserver/server.js
+++ node/webserver/server.js
@@ -6,7 +6,28 @@ const fs = require('fs')
 const express = require('express')
 const serverIndex = require('serve-index')
 const socketIO = require('socket.io')
+const log4js = require('log4js')
 
+log4js.configure({
+    appenders: {
+      file: {
+        type: 'file',
+        filename: 'app.log',
+        layout: {
+            type: 'pattern',
+            pattern: '%r %p - %m',
+        }
+      }
+    },
+    categories: {
+        default: {
+            appenders: ['file'],
+            level: 'debug'
+        }
+    }
+})
+
+var logger = log4js.getLogger()
 var app = express()
 app.use(serverIndex('./public'))
 app.use(express.static('./public'))
@@ -30,6 +51,7 @@ io.sockets.on('connection',(socket) => {
       socket.join(room)
       var myRoom = io.sockets.adapter.rooms[room]
       var userCount = Object.keys(myRoom.sockets).length
+      logger.log('the number of user in room is: ' + userCount)
       /// 向当前用户回复消息
       /// socket.emit("joined", room, socket.id)
       /// 向房间中除当前加入用户之外的所有用户回复消息
@@ -42,9 +64,11 @@ io.sockets.on('connection',(socket) => {
 
     
     socket.on('leave',(room) => {
-      socket.leave(room)
       var myRoom = io.sockets.adapter.rooms[room]
       var userCount = Object.keys(myRoom.sockets).length
+
+      logger.log('the number of user in room is: ' + userCount -1)
+      socket.leave(room)
       /// 向当前用户回复消息
       /// socket.emit("joined", room, socket.id)
       /// 向房间中除当前加入用户之外的所有用户回复消息
