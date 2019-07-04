6d652e62319d9d119afd065c5d4ee720003b7949
🐯🍎💦🌟🍀 <birneysky@163.com>
2019-07-05 00:01:36 +0800

    make a litte for draw paths

---
+++ SwfitUI/Landmarks/SwfitUI/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwfitUI
+//
+//  Created by birneysky on 2019/7/4.
+//  Copyright © 2019 COOL. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct RotatedBadgeSymbol_Previews : PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol()
+    }
+}
+#endif
