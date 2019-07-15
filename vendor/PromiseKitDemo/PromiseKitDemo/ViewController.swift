d1df012519fa8e753f77a78d6456c4f1ddcbfbef
zhaobingdong🌲🌱🌿💥 <zhaobingdong@rongcloud.cn>
2019-07-15 18:52:52 +0800

    promisekit

---
+++ vendor/PromiseKitDemo/PromiseKitDemo/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  PromiseKitDemo
+//
+//  Created by birney on 2019/7/15.
+//  Copyright © 2019 RongCloud. All rights reserved.
+//
+
+import UIKit
+
+struct User {
+    var name: String
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        firstly {
+            login()
+        }.done { (_) in
+                
+        }
+    }
+
+    func login() -> Promise<()> {
+        return Promise { (resolver) in
+                resolver.fulfill(())
+        }
+
+    }
+
+
+}
+
