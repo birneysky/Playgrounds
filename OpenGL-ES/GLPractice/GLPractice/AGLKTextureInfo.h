d0959a777968f636cc03ea041b70d068285e2503
🐯🍎💦🌟🍀 <birneysky@163.com>
2019-07-28 16:50:17 +0800

    add custom GLKTextinfo

---
+++ OpenGL-ES/GLPractice/GLPractice/AGLKTextureInfo.h
@@ -0,0 +1,17 @@
+//
+//  AGLKTextureInfo.h
+//  GLPractice
+//
+//  Created by birneysky on 2019/7/27.
+//  Copyright © 2019 Grocery. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AGLKTextureInfo : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OpenGL-ES/GLPractice/GLPractice/AGLKTextureInfo.m
@@ -0,0 +1,13 @@
+//
+//  AGLKTextureInfo.m
+//  GLPractice
+//
+//  Created by birneysky on 2019/7/27.
+//  Copyright © 2019 Grocery. All rights reserved.
+//
+
+#import "AGLKTextureInfo.h"
+
+@implementation AGLKTextureInfo
+
+@end
