f68212c6e8e376370e487e02e27b390a8d967366
🐯🍎💦🌟🍀 <birneysky@163.com>
2020-05-21 01:27:39 +0800

    kata

--- kata/c++/ReverseList.cpp
+++ kata/c++/ReverseList.cpp
@@ -1,13 +1,26 @@
 #include"ListNode.hpp"
 
 ListNode* ReverseList(ListNode* pHead) {
-
-	return nullptr;
+	ListNode* pre = nullptr;
+	ListNode* cur = pHead;
+	while(cur) {
+		ListNode* next = cur->next;
+		cur->next = pre;
+		pre = cur;
+		cur = next;
+		if(next)
+			next = next->next;
+	}
+	return pre;
 }
 
 
 int main(int argc, char* argv[]) {
-	ReverseList(nullptr);
+	std::vector<int> nums1{1, 2, 4, 5, 6, 7, 9};
+	ListNode* l1 = new ListNode(nums1);
+	std::cout << l1 << std::endl;
+	ListNode* l2 = ReverseList(l1);
+	std::cout << l2;
 }
 
 
--- kata/c++/ReverseSentence.cpp
+++ kata/c++/ReverseSentence.cpp
@@ -1,13 +1,54 @@
+#include<iostream>
 
 void Reverse(char* pBegin, char* pEnd) {
-
+	if (!pBegin || !pEnd) {
+		return;
+	}
+	while (pBegin < pEnd) {
+		char temp = *pBegin;
+		*pBegin = *pEnd;
+		*pEnd = temp;
+		pBegin ++;
+		pEnd --;
+	}
 }
 
-char* ReverseSentence(const char* pData) {
-	return nullptr;
+char* ReverseSentence(char* pData) {
+	if (!pData) {
+		return nullptr;
+	}
+
+	char* pBegin = pData;
+	char* pEnd = pBegin;
+	while (pBegin < pEnd) {
+		char temp = *pBegin;
+		*pBegin = *pEnd;
+		*pEnd = temp;
+		pBegin ++;
+		pEnd --;
+	}
+	Reverse(pBegin, ++pEnd);
+
+	pBegin = pEnd = pData;
+	while (pBegin < pEnd) {
+		if (*pBegin == ' ' && *pEnd == ' ') {
+			pBegin ++;
+			pEnd ++;
+		} else if(*pBegin != ' ' && *pEnd == ' ') {
+			Reverse(pBegin, --pEnd);
+			pBegin = ++ pEnd;
+		} else {
+			pEnd ++;
+		}
+	}
+
+	return pData;
 }
 
 
 int main(int argc, char* argv[]) {
-	ReverseSentence("abc");
+//	ReverseSentence("abc");
+	char pBegin[] = "are you ok";
+	ReverseSentence(pBegin);
+	std::cout << pBegin;
 }
