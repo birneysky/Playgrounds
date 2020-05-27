aa79c6e4c96415e55ac04b343279fb0fa2624267
🐯🍎💦🌟🍀 <birneysky@163.com>
2020-05-28 00:38:26 +0800

    a swift script prototype

---
+++ kata/swift/Script.swift
@@ -0,0 +1,42 @@
+import Foundation
+/// https://github.com/kareman/SwiftShell
+/// https://blog.csdn.net/u011865919/article/details/89415390
+
+@discardableResult
+func run(_ command:String, arguments:[String]? = nil) -> (Int32) {
+	let process = Process()
+	process.launchPath = command 
+	if let argv = arguments {
+		process.arguments = argv
+	}
+	process.launch()
+	process.waitUntilExit()
+	return (process.terminationStatus)
+}
+
+func exec(_ command: String, arguments:[String]? = nil) ->(Int32, String?) {
+	let process = Process()
+    process.launchPath = command;
+    if let argv = arguments {
+        process.arguments = argv
+    }
+    let pipe = Pipe()
+    process.standardOutput = pipe
+    process.launch()
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output: String? = String(data: data, encoding: .utf8)
+    process.waitUntilExit()
+    pipe.fileHandleForReading.closeFile()
+    return (process.terminationStatus, output)
+}
+
+let enviroment = ProcessInfo.processInfo.environment
+if let shell = enviroment["SHELL"] {
+	print("SHELL is \(shell)")
+}
+run("/usr/bin/say", arguments: ["ladies and gentlemen, may i have your attention please, i have a very import announcement to make"])
+let  (status, message) = exec("/usr/bin/whereis",arguments: ["ls"])
+guard status == 0, let msg = message else {
+	fatalError()
+}
+print("status:\(status), \(msg)")
