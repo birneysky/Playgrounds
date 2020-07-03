e992e79a8a6ef2a909cce11148ac51bb83a325fd
zhaobingdong🌲🌱🌿💥 <zhaobingdong@rongcloud.cn>
2020-07-03 17:44:28 +0800

    alter test case

--- kata/c++/KataTest/BinarySearchTest.mm
+++ kata/c++/KataTest/BinarySearchTest.mm
@@ -26,12 +26,11 @@
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
     std::vector<int> nums1{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    assert(binarySearch(nums1, 3) == 3);
+    XCTAssertEqual(binarySearch(nums1, 3), 3);
+   
     std::vector<int> nums2{10, 30, 45, 67, 89, 90, 91, 92, 93, 94};
-    assert(binarySearch(nums2, 30) == 1);
-    assert(binarySearch(nums1, 11) == -1);
-
-
+    XCTAssertEqual(binarySearch(nums2, 30), 1);
+    XCTAssertEqual(binarySearch(nums2, 11), -1);
 }
 
 - (void)testPerformanceExample {
