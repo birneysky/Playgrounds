79502c88df997453d3367dba535f21481e5e3d3d
zhaobingdong🌲🌱🌿💥 <zhaobingdong@rongcloud.cn>
2020-08-17 14:40:14 +0800

    practice rever list again

--- kata/c++/KataTest/ReverseListTest.mm
+++ kata/c++/KataTest/ReverseListTest.mm
@@ -22,15 +22,48 @@
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)testExample {
+- (void)testReverseList1 {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-    std::vector<int> nums1{1, 2, 4, 5, 6, 7, 9};
-    ListNode* l1 = new ListNode(nums1);
-    std::cout << l1 << std::endl;
-    ListNode* l2 = ReverseList(l1);
-    std::cout << l2;
+    std::vector<int> nums{1, 2, 4, 5, 6, 7, 9};
+    ListNode* list = new ListNode(nums);
+    std::cout << list << std::endl;
+    ListNode* head = ReverseList(list);
+    std::cout << head;
+    for (size_t i = nums.size() - 1; i >= 0 && head != nullptr; i --) {
+         XCTAssertEqual(head->val, nums[i]);
+         head = head->next;
+    }
 }
+
+/// 空链表
+- (void)testReverseList2 {
+    ListNode* head = ReverseList(nullptr);
+    XCTAssertEqual(head, nullptr);
+}
+
+/// 只有一个元素
+- (void)testReverseList3 {
+    ListNode* list = new ListNode(3);
+    std::cout << list << std::endl;
+    ListNode* head = ReverseList(list);
+    std::cout << head;
+    XCTAssertEqual(head->val, 3);
+}
+
+/// 2 个元素
+- (void)testReverseList4 {
+    std::vector<int> nums {2, 5};
+    ListNode* list = new ListNode(nums);
+    std::cout << list << std::endl;
+    ListNode* head = ReverseList(list);
+    std::cout << head;
+    for (size_t i = nums.size() - 1; i >= 0 && head != nullptr; i --) {
+        XCTAssertEqual(head->val, nums[i]);
+        head = head->next;
+    }
+}
+
 
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
--- kata/c++/ReverseList.cpp
+++ kata/c++/ReverseList.cpp
@@ -1,17 +1,15 @@
 #include"ListNode.hpp"
 
 ListNode* ReverseList(ListNode* pHead) {
-	ListNode* pre = nullptr;
-	ListNode* cur = pHead;
-	while(cur) {
-		ListNode* next = cur->next;
-		cur->next = pre;
-		pre = cur;
-		cur = next;
-		if(next)
-			next = next->next;
-	}
-	return pre;
+    ListNode* pre = nullptr;
+    ListNode* cur = pHead;
+    while (cur != nullptr) {
+        ListNode* next = cur->next;
+        cur->next = pre;
+        pre = cur;
+        cur = next;
+    }
+    return pre;
 }
 
 
