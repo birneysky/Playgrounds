3a8f2bc6e77c2c035aec8ffa837daaa335c1f7d0
🐯🍎💦🌟🍀 <birneysky@163.com>
2020-08-19 01:44:05 +0800

    Coins problem explore

---
+++ kata/c++/KataHelper.cpp
@@ -0,0 +1,8 @@
+//
+//  KataHelper.cpp
+//  KataTest
+//
+//  Created by birneysky on 2020/8/18.
+//
+
+#include "KataHelper.hpp"
---
+++ kata/c++/KataHelper.hpp
@@ -0,0 +1,13 @@
+//
+//  KataHelper.hpp
+//  KataTest
+//
+//  Created by birneysky on 2020/8/18.
+//
+
+#ifndef KataHelper_hpp
+#define KataHelper_hpp
+
+#include <stdio.h>
+
+#endif /* KataHelper_hpp */
