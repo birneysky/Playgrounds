a2962bdee6e655fa8deb08533fdd5b1f0b16ef58
Freddy <afreddyshere@gmail.com>
2023-10-26 00:05:51 +0800

    swift ui boot camp: initializer

---
+++ SwiftUI/SwiftUIBootCamp/SwiftUIBootCamp/Initializers.swift
@@ -0,0 +1,20 @@
+//
+//  Initializers.swift
+//  SwiftUIBootCamp
+//
+//  Created by Bruce on 2023/10/25.
+//
+
+import SwiftUI
+
+struct Initializers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Initializers_Previews: PreviewProvider {
+    static var previews: some View {
+        Initializers()
+    }
+}
