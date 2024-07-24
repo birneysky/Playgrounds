6dee54825e3271111bf172e9ac49ca9071c9af81
zhaobingdong <zhaobingdong@changba.com>
2024-07-24 19:46:32 +0800

    test code

--- TestXcodePreview/TestXcodePreview/TestImageViewController.swift
+++ TestXcodePreview/TestXcodePreview/TestImageViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TestImageViewController: UIViewController {
+class TestImageViewController: UIViewController, UITextViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +26,60 @@ class TestImageViewController: UIViewController {
     }
     */
 
+    func checkxxxx(text: String) -> Bool {
+        let pattern = ".*#([^#]+#)+.*"
+        
+        do {
+            let regex = try NSRegularExpression(pattern: pattern)
+            let range = NSRange(location: 0, length: text.utf16.count)
+            
+            if let _ = regex.firstMatch(in: text, options: [], range: range) {
+                return true
+            } else {
+                return false
+            }
+        } catch {
+            print("Invalid regex: \(error.localizedDescription)")
+            return false
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        guard let textRange = Range(range, in: textView.text) else { return false }
+        let updatedText = textView.text.replacingCharacters(in: textRange, with: text)
+        let cursorPosition = range.location + (text as NSString).length
+        let searchRange =  Range(NSRange(location: 0, length: cursorPosition), in: updatedText)
+        // 查找光标前的最后一个 #
+        if let precedingHashRange = updatedText.range(of: "#", options: .backwards, range: searchRange, locale: nil) {
+            
+
+            
+            //let precedingText = updatedText[precedingHashRange]//(currentText as NSString).substring(from: precedingHashRange.location + 1)
+            let precedingText = updatedText[precedingHashRange.lowerBound...]
+            print("##### \(precedingText) \(updatedText)")
+            if let nextHashRange = precedingText.range(of: "#") {
+                // 两个 # 之间的内容
+                //let content = precedingText[nextHashRange]//(precedingText as NSString).substring(to: nextHashRange.location)
+                let content = precedingText[nextHashRange.upperBound...]
+                
+                if content.count <= 0, checkxxxx(text: updatedText) {
+                    print("#####  dismiss")
+                    return true
+                }
+                //showPanel(withText: content)
+                print("#####  show one content \(content)")
+            } else {
+                // 光标前有一个 #
+                //showPanel(withText: precedingText)
+                print("#####  show two content \(precedingText)")
+            }
+        } else {
+            // 光标前没有 #
+            ///hidePanel()
+            print("#####  dismiss")
+        }
+        return true;
+    }
 }
 
 #Preview {
